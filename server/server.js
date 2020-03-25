8227fe22b9bec2b3bb7e1e0700168193ea75416b
Raiyol <dna-@live.fr>
2020-03-25 19:15:20 +0100

    sparql

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
-const {PORT} = require('./constants');
+const { PORT } = require('./constants');
 const sparql = require('./query_db');
 const queryville = require('./query_ville');
 
@@ -13,27 +13,29 @@ server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
 server.use(cors());
 server.use(bodyParser.json());
-server.use(bodyParser.urlencoded({extended : true}));
+server.use(bodyParser.urlencoded({ extended: true }));
+
+server.set('views', path.join(__dirname, '../views'));
+server.get('/sparql', function(req, res) {
+	console.log('trying');
+	res.render('sparql.ejs', { result: null, query: '' });
+});
 
 server.use(express.static(path.join(__dirname, '../public')));
 server.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-server.get('/sparql', function(req, res){
-  res.render('sparql.ejs', {result : null, query : ''})
-});
-
-server.post('/sparql', function(req, res){
-  console.log(req.body.query);
-  sparql.get(req.body.query).then(function(exit){
-    console.log(exit);
-    res.render('sparql.ejs', {result : exit, query : req.body.query});
-  });
+	res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 server.get('/api/:ville', (req, res) => {
-  queryville(req.params.ville).then(function(result){
-    res.send({express : result});
-  });
+	queryville(req.params.ville).then(function(result) {
+		res.send({ express: result });
+	});
+});
+
+server.post('/sparql', function(req, res) {
+	console.log(req.body.query);
+	sparql.get(req.body.query).then(function(exit) {
+		console.log(exit);
+		res.render('sparql.ejs', { result: exit, query: req.body.query });
+	});
 });
