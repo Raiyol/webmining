5720cda30dcaa642b925d1277f96fdc4b707f21a
Raiyol <dna-@live.fr>
2020-03-25 19:25:07 +0100

    query

--- server/query_db.js
+++ server/query_db.js
@@ -1,28 +1,22 @@
 const axios = require('axios');
-const {DB_URL} = require('./constants');
+const { DB_URL } = require('./constants');
 const querystring = require('querystring');
 
-module.exports.get = async q => {
-    try {
-        const response = await axios.post(DB_URL, querystring.stringify({
-            query : q
-        }));
-        const {data, status} = response;
-        if (status >= 200 && status < 300) {
-            return data;
-        }
-        console.error(status);
-        return null;
-    } catch (error) {
-        return null;
-    }
+module.exports.get = async (q) => {
+	try {
+		const response = await axios.post(
+			DB_URL,
+			querystring.stringify({
+				query: q
+			})
+		);
+		const { data, status } = response;
+		if (status >= 200 && status < 300) {
+			return data;
+		}
+		console.error(status);
+		return null;
+	} catch (error) {
+		return null;
+	}
 };
-
-/*
-
-querying('SELECT ?subject ?predicate ?object WHERE { ?subject ?predicate ?object } LIMIT 50')
-.then(function(result){
-    console.log(result.results.bindings);
-});
-
-*/
